c917d7be3b5dd61927429ad5b4ad59021235ad39
sesor33 <sesor333@aol.com>
2021-11-15 17:06:47 -0500

    Default background asset positions edited

--- mods/stages/phila.lua
+++ mods/stages/phila.lua
@@ -2,39 +2,36 @@
 
 function onCreate()
 	-- background shit
-	makeLuaSprite('BG1', 'phila/Layer 09_Phila BG 3', -640, -400);
+	makeLuaSprite('BG1', 'phila/Layer 09_Phila BG 3', -550, -350);
 	setScrollFactor('BG1', 0.1, 0.1);
 
-	makeAnimatedLuaSprite('hex','phila/Layer 08_Phila SnowHex Loop', 1200, 0);
+	makeAnimatedLuaSprite('hex','phila/Layer 08_Phila SnowHex Loop', 1650, 70);
 	addAnimationByPrefix('hex','bump','Layer 08_Phila SnowHex Loop', 24, false);
-	setScrollFactor('hex', 0.5, 0.5);
+	setScrollFactor('hex', 1, 1);
 	scaleObject('hex', 1, 1);
 
-	makeAnimatedLuaSprite('golinLoop','phila/Layer 07_Phila Golin REC Loop', -200, -15);
+	makeAnimatedLuaSprite('golinLoop','phila/Layer 07_Phila Golin REC Loop', -200, 90);
 	addAnimationByPrefix('golinLoop','bump','Layer 07_Phila Golin REC Loop', 24, false);
-	setScrollFactor('golinLoop', 0.5, 0.5);
 	scaleObject('golinLoop', 1, 1);
 
-	makeLuaSprite('BG2', 'phila/Layer 06_Phila BG 2', -640, -450);
-	setScrollFactor('BG2', 0.5, 0.5);
+	makeLuaSprite('BG2', 'phila/Layer 06_Phila BG 2', -550, -350);
+	setScrollFactor('BG2', 1, 1);
 
-	makeLuaSprite('BG3', 'phila/Layer 05_Phila BG 1', -640, -400);
-	setScrollFactor('BG3', 0.6, 0.6);
+	makeLuaSprite('BG3', 'phila/Layer 05_Phila BG 1', -550, -350);
+	setScrollFactor('BG3', 1, 1);
 
-	makeLuaSprite('BG4', 'phila/Layer 04_Phila Croud Ground', -640, -350);
-	setScrollFactor('BG4', 0.7, 0.7);
+	makeLuaSprite('BG4', 'phila/Layer 04_Phila Croud Ground', -550, -350);
+	setScrollFactor('BG4', 1, 1);
 
-	makeAnimatedLuaSprite('crowd','phila/Layer 03_Phila Croud', 0, 450);
+	makeAnimatedLuaSprite('crowd','phila/Layer 03_Phila Croud', -275, 250);
 	addAnimationByPrefix('crowd','bump','Layer 03_Phila Croud', 24, false);
 	setScrollFactor('crowd', 1, 1);
-	scaleObject('crowd', 0.85, 0.85);
-
 	
-	makeLuaSprite('FG', 'phila/Layer 01_Phila FG 1', -550, -275);
+	makeLuaSprite('FG', 'phila/Layer 01_Phila FG 1', -550, -350);
 	setScrollFactor('FG', 1, 1);
 	scaleObject('FG', 1, 1);
 
-	makeLuaSprite('FG2', 'phila/Layer 02_Phila FG 2', -640, -275);
+	makeLuaSprite('FG2', 'phila/Layer 02_Phila FG 2', -550, -350);
 	setScrollFactor('FG2', 1, 1);
 	scaleObject('FG2', 1, 1);
 	
