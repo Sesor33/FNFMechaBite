37d2b61737fe2e5b6fc665fc81976661aac0a3f0
sesor33 <sesor333@aol.com>
2021-11-28 17:31:53 -0500

    Sprite sheet updated, depth added to phila stage

--- mods/stages/phila.lua
+++ mods/stages/phila.lua
@@ -5,27 +5,28 @@ function onCreate()
 	makeLuaSprite('BG1', 'phila/Layer 09_Phila BG 3', -550, -350);
 	setScrollFactor('BG1', 0.1, 0.1);
 
-	makeAnimatedLuaSprite('hex','phila/Layer 08_Phila SnowHex Loop', 1650, 70);
+	makeAnimatedLuaSprite('hex','phila/Layer 08_Phila SnowHex Loop', 1500, 70);
 	addAnimationByPrefix('hex','bump','Layer 08_Phila SnowHex Loop', 24, false);
-	setScrollFactor('hex', 1, 1);
+	setScrollFactor('hex', 0.7, 0.7);
 	scaleObject('hex', 1, 1);
 
 	makeAnimatedLuaSprite('golinLoop','phila/Layer 07_Phila Golin REC Loop', -200, 35);
 	addAnimationByPrefix('golinLoop','bump','Layer 07_Phila Golin REC Loop', 24, false);
+	setScrollFactor('golinLoop', 0.7, 0.7);
 	scaleObject('golinLoop', 1, 1);
 
 	makeLuaSprite('BG2', 'phila/Layer 06_Phila BG 2', -550, -400);
-	setScrollFactor('BG2', 1, 1);
+	setScrollFactor('BG2', 0.7, 0.7);
 
 	makeLuaSprite('BG3', 'phila/Layer 05_Phila BG 1', -550, -350);
-	setScrollFactor('BG3', 1, 1);
+	setScrollFactor('BG3', 0.8, 0.8);
 
 	makeLuaSprite('BG4', 'phila/Layer 04_Phila Croud Ground', -550, -350);
-	setScrollFactor('BG4', 1, 1);
+	setScrollFactor('BG4', 0.9, 0.9);
 
 	makeAnimatedLuaSprite('crowd','phila/Layer 03_Phila Croud', -275, 250);
 	addAnimationByPrefix('crowd','bump','Layer 03_Phila Croud', 24, false);
-	setScrollFactor('crowd', 1, 1);
+	setScrollFactor('crowd', 0.9, 0.9);
 	
 	makeLuaSprite('FG', 'phila/Layer 01_Phila FG 1', -550, -350);
 	setScrollFactor('FG', 1, 1);
